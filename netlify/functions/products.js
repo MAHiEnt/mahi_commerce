87cd8dc53cbc1db48dcaae07fdf7566c5a01201e
MAHiEnt <mahimalcolm@protonmail.com>
2025-11-02 03:50:06 -0500

    Delete netlify/functions directory

--- netlify/functions/products.js
+++
@@ -1,9 +0,0 @@
-// This file serves your product list.
-const products = require('../../products.json');
-
-exports.handler = async () => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(products),
-  };
-};
